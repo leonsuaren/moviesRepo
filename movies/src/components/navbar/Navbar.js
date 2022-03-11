1b6bf881f51870e205c84284ef5f6c25501df012
arpitadeshpande <deshpande_arpita95@yahoo.com>
2022-03-10 17:08:11 -0800

    MM-11 - Developed Navbar

---
+++ movies/src/components/navbar/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+export const Navbar= () =>{
+ return (
+  <nav class="navbar navbar-expand-lg navbar-light bg-light" data-test="navbar-component">
+  <div class="container-fluid">
+    <Link to='/' class="navbar-brand">Movies Demo</Link>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">  
+      </ul>
+   </div>
+  </div>
+</nav>
+
+ )
+}
