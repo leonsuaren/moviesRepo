2c7237c1be5db8244593f639091c5a6f5f55942c
Leon Suarez <leonsua@gmail.com>
2022-03-12 17:19:58 -0500

    remove temporary content and comment temporary home

--- movies/src/App.js
+++ movies/src/App.js
@@ -3,8 +3,6 @@ import { Navbar } from './components/navbar/Navbar';
 import { Footer } from './components/footer/Footer';
 import { BrowserRouter as Router,Routes , Route } from 'react-router-dom';
 
-import { TemporaryHome } from './views/TemporaryHome';
-
 function App() {
   return (
     <div>
