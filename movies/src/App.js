203ce37ceb016aca3d69e268347b322eae8877cb
apatel544 <ankurpatel544@gmail.com>
2022-03-13 15:22:13 -0400

    Home Page carousel

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,15 +1,17 @@
 import './App.css';
 import { Navbar } from './components/navbar/Navbar';
+import { Home } from './components/home/Home';
 import { Footer } from './components/footer/Footer';
-import { BrowserRouter as Router,Routes , Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
-    <Router>
-     <Navbar />
-     <Footer />
-     </Router>
+      <Router>
+        <Navbar />
+        <Home />
+        <Footer />
+      </Router>
     </div>
   );
 }
