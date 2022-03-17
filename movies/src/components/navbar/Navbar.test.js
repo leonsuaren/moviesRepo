9f8d69cec117a901b88395f9ed2f504527e337e0
Leon Suarez <leonsua@gmail.com>
2022-03-16 21:14:31 -0400

    change classes name

--- movies/src/components/navbar/Navbar.test.js
+++ movies/src/components/navbar/Navbar.test.js
@@ -3,25 +3,25 @@ import {Navbar} from './Navbar';
 
 
 test('render navbar',()=>{
-const result = shallow(<Navbar />);
-const navbarComponent = result.find("[data-test='navbar-component']");
+const wrapper = shallow(<Navbar />);
+const navbarComponent = wrapper.find("[data-test='navbar-component']");
 expect(navbarComponent.length).toBe(1);
 });
 
 test('renders favorites link', () => {
-    const result = shallow(<Navbar />);
-    const favoritesComponent = result.find("[data-test='favorites-component']");
+    const wrapper = shallow(<Navbar />);
+    const favoritesComponent = wrapper.find("[data-test='favorites-component']");
     expect(favoritesComponent.length).toBe(1);
 });
 
 test('renders actors link', () => {
-    const result = shallow(<Navbar />);
-    const actorsLinkComponent= result.find("[data-test='actors-link-component']");
+    const wrapper = shallow(<Navbar />);
+    const actorsLinkComponent= wrapper.find("[data-test='actors-link-component']");
     expect(actorsLinkComponent.length).toBe(1);
 });
 
 test('renders now-playing link', () => {
-    const result = shallow(<Navbar />);
-    const nowPlayingComponent= result.find("[data-test='now-playing-component']");
+    const wrapper = shallow(<Navbar />);
+    const nowPlayingComponent= wrapper.find("[data-test='now-playing-component']");
     expect(nowPlayingComponent.length).toBe(1);
 });
