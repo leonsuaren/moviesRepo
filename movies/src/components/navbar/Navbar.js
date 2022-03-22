8dd1e9550dbcdeeb2119e679425749c94bdda7fb
Honojerame <preciousonoj@gmai.com>
2022-03-21 19:06:54 -0600

    changes to menu

--- movies/src/components/navbar/Navbar.js
+++ movies/src/components/navbar/Navbar.js
@@ -1,23 +1,120 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from "react";
+import { Link } from "react-router-dom";
 
-export const Navbar= () =>{
- return (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light" data-test="navbar-component">
-  <div className="container-fluid">
-    <Link to='/' className="navbar-brand">Movies Demo</Link>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarSupportedContent">
-      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li><Link to='actors' className='nav-link' data-test='actors-link-component'>Actors</Link></li>
-          <li><Link to='movie-favorites' className='nav-link' data-test='favorites-component'>Favorites</Link></li>
-          <li className="nav-item"><Link to='now-playing' className='nav-link active' data-test='now-playing-component'>Now Playing</Link></li>
-      </ul>
-   </div>
-  </div>
-</nav>
-
- )
-}
+export const Navbar = () => {
+  return (
+    <nav
+      className="navbar navbar-expand-lg navbar-light bg-light"
+      data-test="navbar-component"
+    >
+      <div className="container-fluid">
+        <Link to="/" className="navbar-brand">
+          Movies Demo
+        </Link>
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav">
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDarkDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                Movies
+              </a>
+              <ul
+                class="dropdown-menu dropdown-menu-dark"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+                <li>
+                  <Link
+                    to="popular"
+                    className="dropdown-item"
+                    data-test="actors-link-component"
+                  >
+                    Popular
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="movie-favorites"
+                    className="dropdown-item"
+                    data-test="favorites-component"
+                  >
+                    Favorites
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="now-playing"
+                    className="dropdown-item"
+                    data-test="now-playing-component"
+                  >
+                    Now Playing
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="upcoming"
+                    className="dropdown-item"
+                    data-test="favorites-component"
+                  >
+                    Upcoming
+                  </Link>
+                </li>
+                <li>
+                  <Link
+                    to="top-rated"
+                    className="dropdown-item"
+                    data-test="favorites-component"
+                  >
+                    Top rated
+                  </Link>
+                </li>
+              </ul>
+            </li>
+            <li class="nav-item dropdown">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDarkDropdownMenuLink"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
+                People
+              </a>
+              <ul
+                class="dropdown-menu dropdown-menu-dark"
+                aria-labelledby="navbarDarkDropdownMenuLink"
+              >
+                <li>
+                  <Link
+                    to="actors"
+                    className="dropdown-item"
+                    data-test="favorites-component"
+                  >
+                    Actors
+                  </Link>
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
