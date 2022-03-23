3c66e6dbfde427acd0bc4e5cc6a0cbb1b4cd2133
Leon Suarez <leonsua@gmail.com>
2022-03-23 18:37:48 -0400

    added now-playing route

--- movies/src/App.js
+++ movies/src/App.js
@@ -4,6 +4,7 @@ import { Home } from './components/home/Home';
 import { Footer } from './components/footer/Footer';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Favorites } from './views/Favorites/Favorites';
+import { NowPlaying } from '../src/components/nowplaying/NowPlaying';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />}/>
           <Route path="favorites" element={<Favorites />}/>
+          <Route path="now-playing" element={<NowPlaying />} />
         </Routes>
         <Footer />
       </Router>
